34892a76c5992247bbbe589d0ae894f43aaabc91
Kelsey Hyde <kelseyhyde@gmail.com>
2015-08-29 09:41:47 +1200

    Create alternative layout

--- app/src/main/java/com/example/smarttrain/smarttrain/MainActivity.java
+++ app/src/main/java/com/example/smarttrain/smarttrain/MainActivity.java
@@ -6,14 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
 
     public void createButtonOnClick(View view) {
