9d5693f227f6aed3f8ea47288d821d7169bb74f6
Kelsey Hyde <kelseyhyde@gmail.com>
2015-09-08 15:05:50 +1200

    Add constructors, mutators, accessors to TimeExercise

--- app/src/main/java/com/example/smarttrain/smarttrain/TimeExercise.java
+++ app/src/main/java/com/example/smarttrain/smarttrain/TimeExercise.java
@@ -4,5 +4,42 @@ package com.example.smarttrain.smarttrain;
  * Created by kelseyhyde on 8/09/15.
  */
 public class TimeExercise extends Exercise {
-    
+    double time;
+    String unit;
+
+    TimeExercise( String name, String description, String unit ){
+        super(name, description);
+        this.unit = unit;
+        this.time = 0.00;
+    }
+
+    TimeExercise( String name, String description, String unit, double time ){
+        super(name, description);
+        this.unit = unit;
+        this.time = time;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime( double time ){
+        this.time = time;
+    }
+
+    public void incrementTime(){
+        time++;
+    }
+
+    public void decrementTime(){
+        time--;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit( String unit ){
+        this.unit = unit;
+    }
 }
