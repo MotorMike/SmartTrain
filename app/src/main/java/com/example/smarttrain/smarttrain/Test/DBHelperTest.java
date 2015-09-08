ad4c4da5c73d4112c0dd1d8864c4f2c63e9358c8
MotorMike <mike.nicholls@outlook.com>
2015-09-08 22:59:18 +1200

    Removed unneeded Exercise test class Started test class for DB (DBHelperTest)

---
+++ app/src/main/java/com/example/smarttrain/smarttrain/Test/DBHelperTest.java
@@ -0,0 +1,48 @@
+package com.example.smarttrain.smarttrain.Test;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Mike on 8/09/2015.
+ */
+public class DBHelperTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testAddExercise() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteExercise() throws Exception {
+
+    }
+
+    @Test
+    public void testOnUpgrade() throws Exception {
+
+    }
+
+    @Test
+    public void testExerciseNameTableToString() throws Exception {
+
+    }
+}
