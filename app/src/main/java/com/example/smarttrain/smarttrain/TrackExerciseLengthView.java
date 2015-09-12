0e087d7b548e837fb3291b9c7666249018ae34ef
MotorMike <mike.nicholls@outlook.com>
2015-09-12 12:34:57 +1200

    Builing of Tack Length Exercise from not yet complete

---
+++ app/src/main/java/com/example/smarttrain/smarttrain/TrackExerciseLengthView.java
@@ -0,0 +1,81 @@
+package com.example.smarttrain.smarttrain;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class TrackExerciseLengthView extends AppCompatActivity {
+
+    Exercise exercise;
+    LengthExercise lengthExercise;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_exercise_length_view);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        displayExerciseTest();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_track_exercise_length_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void confirmButtonOnClick(View view) {
+
+    }
+
+
+    //Get exercise from data base
+    public void loadExercise() {
+        displayExercise();
+    }
+
+    //
+    public void displayExercise() {
+
+        TextView valueTextView = (TextView) findViewById(R.id.valueTextView);
+        TextView unitTextView = (TextView) findViewById(R.id.unitTextView);
+        TextView unitsTextView2 = (TextView) findViewById(R.id.unitsTextView2);
+        TextView exerciseNameTextView = (TextView) findViewById(R.id.exerciseNameTextView);
+        TextView textDescriptionTextView = (TextView) findViewById(R.id.textDescriptionTextView);
+
+
+        unitTextView.setText(lengthExercise.getUnit());
+        unitsTextView2.setText(lengthExercise.getUnit());
+        exerciseNameTextView.setText(lengthExercise.getName());
+        textDescriptionTextView.setText(lengthExercise.getDescription());
+
+    }
+
+    public void displayExerciseTest() {
+        lengthExercise = new LengthExercise("Run", "One leg infont of another", "Kilometers");
+        displayExercise();
+
+    }
+
+
+}
